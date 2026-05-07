0c0c6a5ca75e9929fe4fd2afd7a6655658ddbc57
freebooz <365256100@qq.com>
2026-05-08 00:26:57 +0800

    2

---
+++ Source/GameCore/Private/GameCoreModule.cpp
@@ -0,0 +1,9 @@
+// Copyright Freebooz Games, Inc. All Rights Reserved.
+
+#include "Modules/ModuleManager.h"
+
+class FGameCoreModule final : public IModuleInterface
+{
+};
+
+IMPLEMENT_MODULE(FGameCoreModule, GameCore)
---
+++ Source/GameMoba/Private/GameMobaModule.cpp
@@ -0,0 +1,9 @@
+// Copyright Freebooz Games, Inc. All Rights Reserved.
+
+#include "Modules/ModuleManager.h"
+
+class FGameMobaModule final : public IModuleInterface
+{
+};
+
+IMPLEMENT_MODULE(FGameMobaModule, GameMoba)
